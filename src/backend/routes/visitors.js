6aa789a43dcf27b0e52b539473a88848c620cb35
Tesals <akinluaolorunfunminiyi@gmail.com>
2025-03-15 20:15:35 +0100

    fis

--- src/backend/routes/visitors.js
+++ src/backend/routes/visitors.js
@@ -17,7 +17,7 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'church-visitors',
-    allowed_formats: ['*'],
+    allowed_formats: ['jpg', 'jpeg', 'png', 'gif'],
     transformation: [{ width: 500, height: 500, crop: 'limit' }]
   }
 });
