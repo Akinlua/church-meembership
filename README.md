2f481509604bddba21b7c52b0141906a571042a1
Akinlua <akinluaolorunfunminiyi@gmail.com>
2026-01-14 00:07:23 +0100

    fits

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Church Membership Application
 
-[Download the latest version of the application here](https://drive.google.com/file/d/1E01tleCqb67g2yyIVHX5zWKVWaUAa1gI/view?usp=sharing)
+[Download the latest version of the application here](https://drive.google.com/file/d/1nOkb68gronNcRQHvZ713sN8mO55CGY_T/view?usp=drive_link)
--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <App />
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 ); 
